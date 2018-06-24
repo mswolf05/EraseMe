4b3b1e687368a85b516b8d62a537767286d6fcbc
mswolf05 <39277326+mswolf05@users.noreply.github.com>
2018-06-24 14:14:12 -0400

    establish tweepy connection

--- eraseme.py
+++ eraseme.py
@@ -11,3 +11,11 @@ consumer_key = input("Consumer Key (API Key): ")
 consumer_secret = input("Consumer Secret (API Secret): ")
 access_token = input("Acess Token: ")
 access_token_secret = input("Acess Token Secret: ")
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+timeline = tweepy.Cursor(api.user_timeline).items()

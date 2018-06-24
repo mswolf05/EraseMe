4c6185745d8a6452ac77f26fca80ad229d11245a
mswolf05 <39277326+mswolf05@users.noreply.github.com>
2018-06-24 14:12:37 -0400

    request secret keys from users
    .env is not working on my windows. I also was not able to set environment variables from the command prompt. Outside of instructing the user to update the variables directly in the source code, this is the safest way to access the required sensitive information.

--- eraseme.py
+++ eraseme.py
@@ -6,3 +6,8 @@ import csv
 from tkinter import *
 from datetime import datetime, timedelta
 from subprocess import Popen
+
+consumer_key = input("Consumer Key (API Key): ")
+consumer_secret = input("Consumer Secret (API Secret): ")
+access_token = input("Acess Token: ")
+access_token_secret = input("Acess Token Secret: ")

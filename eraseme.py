a4a24750276ed10912db7200105cdd307251e109
mswolf05 <39277326+mswolf05@users.noreply.github.com>
2018-06-24 14:15:20 -0400

    build list of dictionaries of twitter activity

--- eraseme.py
+++ eraseme.py
@@ -19,3 +19,23 @@ auth.set_access_token(access_token, access_token_secret)
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 timeline = tweepy.Cursor(api.user_timeline).items()
+
+cutoff_date = input("Perform action on all Twitter activity prior to which date (MM/DD/YYYY): ")
+
+cutoff_date = datetime.strptime(cutoff_date, "%m/%d/%Y").strftime("%Y-%m-%d")
+cutoff_date = datetime.strptime(cutoff_date , '%Y-%m-%d')
+
+print("Please wait while we gather your Twitter activity...")
+
+tweet_list = []
+for tweet in timeline:
+    if tweet.created_at <= cutoff_date:
+        info = {
+        "tweet_id": tweet.id,
+        "tweet_text": tweet.text,
+        "tweet_created": tweet.created_at.strftime('%m/%d/%Y'),
+        "retweet": tweet.retweeted,
+        "like": tweet.favorited,
+        "tweet_delete": True
+        }
+        tweet_list.append(info)

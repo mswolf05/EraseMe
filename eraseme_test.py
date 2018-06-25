a975738cda77123d9fc78c2f875ecbbab2fb1283
mswolf05 <39277326+mswolf05@users.noreply.github.com>
2018-06-25 10:18:28 -0400

    second test for date format for tweepy

--- eraseme_test.py
+++ eraseme_test.py
@@ -3,3 +3,8 @@ import eraseme
 def test_timeline_retrieve():
     result = len(tweet_list)
     assert result == 1
+
+def test_date(cutoff_date="06/25/2018"):
+    result = datetime.strptime(cutoff_date, "%m/%d/%Y").strftime("%Y-%m-%d")
+    result = datetime.strptime(cutoff_date, "%Y-%m-%d")
+    assert result == datetime.datetime(2018, 6, 25, 0, 0)

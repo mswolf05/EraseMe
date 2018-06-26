5281deddb28d4baf1130954c63ad66287dd1e6f7
mswolf05 <39277326+mswolf05@users.noreply.github.com>
2018-06-25 20:19:12 -0400

    update to include proper packages and code

--- eraseme_test.py
+++ eraseme_test.py
@@ -1,10 +1,16 @@
 import eraseme
+from datetime import datetime, timedelta
 
 def test_timeline_retrieve():
-    result = len(tweet_list)
-    assert result == 1
+    tweet_list = [{"tweet_id": 1011398736696823809, "tweet_text": "Just setting up my Twitter. #myfirstTweet", "tweet_created": "06/26/2018", "retweet": False, "like": False, "tweet_delete": True}]
+    result = tweet_list[0]["tweet_id"]
+    assert result == 1011398736696823809
 
-def test_date(cutoff_date="06/25/2018"):
+def test_date():
+    cutoff_date="06/25/2018"
+
     result = datetime.strptime(cutoff_date, "%m/%d/%Y").strftime("%Y-%m-%d")
-    result = datetime.strptime(cutoff_date, "%Y-%m-%d")
-    assert result == datetime.datetime(2018, 6, 25, 0, 0)
+    result = datetime.strptime(result, "%Y-%m-%d")
+    
+    assert result == datetime(2018, 6, 25, 0, 0)
+

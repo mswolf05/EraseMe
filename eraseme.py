c878d14bde14eac50a916dce50ec4c5d0e4261af
mswolf05 <39277326+mswolf05@users.noreply.github.com>
2018-06-25 20:18:03 -0400

    structure for proper automated testing

--- eraseme.py
+++ eraseme.py
@@ -8,10 +8,11 @@ from datetime import datetime, timedelta
 from subprocess import Popen
 import pytest
 
-consumer_key = input("Consumer Key (API Key): ")
-consumer_secret = input("Consumer Secret (API Secret): ")
-access_token = input("Acess Token: ")
-access_token_secret = input("Acess Token Secret: ")
+# Consumer keys and access tokens, used for OAuth
+#consumer_key = os.environ.get("consumer_key")# or "OOPS. Please set an environment variable named 'consumer_key'."
+#consumer_secret = os.environ.get("consumer_secret")# or "OOPS. Please set an environment variable named 'consumer_secret'."
+#access_token = os.environ.get("access_token")# or "OOPS. Please set an environment variable named 'access_token'."
+#access_token_secret = os.environ.get("access_token_secret")# or "OOPS. Please set an environment variable named 'access_token_secret'."
 
 def handle_button_click():
     item_id_keep = []
@@ -62,72 +63,79 @@ def handle_button_click():
 
 def handle_button2_click():
     p = Popen("eraseme_activity_report.csv", shell=True)
-    master.destroy()    
+    master.destroy()
 
 def on_closing():
     filename = "eraseme_activity_report.csv"
     if os.path.exists(filename):
         os.remove(filename)
         print("All twitter activity reports removed from the EraseMe application.")
-    master.destroy()    
-    
-# OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+    master.destroy()
 
-# Creation of the actual interface, using authentication
-api = tweepy.API(auth)
-timeline = tweepy.Cursor(api.user_timeline).items()
+if __name__ == '__main__':
 
-cutoff_date = input("Perform action on all Twitter activity prior to which date (MM/DD/YYYY): ")
+    consumer_key = input("Consumer Key (API Key): ")
+    consumer_secret = input("Consumer Secret (API Secret): ")
+    access_token = input("Acess Token: ")
+    access_token_secret = input("Acess Token Secret: ")
 
-cutoff_date = datetime.strptime(cutoff_date, "%m/%d/%Y").strftime("%Y-%m-%d")
-cutoff_date = datetime.strptime(cutoff_date , '%Y-%m-%d')
+    # OAuth process, using the keys and tokens
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-print("Please wait while we gather your Twitter activity...")
+    # Creation of the actual interface, using authentication
+    api = tweepy.API(auth)
+    timeline = tweepy.Cursor(api.user_timeline).items()
 
-tweet_list = []
-for tweet in timeline:
-    if tweet.created_at <= cutoff_date:
-        info = {
-        "tweet_id": tweet.id,
-        "tweet_text": tweet.text,
-        "tweet_created": tweet.created_at.strftime('%m/%d/%Y'),
-        "retweet": tweet.retweeted,
-        "like": tweet.favorited,
-        "tweet_delete": True
-        }
-        tweet_list.append(info)
+    cutoff_date = input("Perform action on all Twitter activity prior to which date (MM/DD/YYYY): ")
 
-# prompt user for which tweets/retweets/likes to keep
-master = Tk()
+    cutoff_date = datetime.strptime(cutoff_date, "%m/%d/%Y").strftime("%Y-%m-%d")
+    cutoff_date = datetime.strptime(cutoff_date, "%Y-%m-%d")
 
-listbox_label = Label(text="Select the twitter activity to KEEP:\n (all unselected tweets, retweets and likes will be deleted)", width=150)
-listbox = Listbox(selectmode=EXTENDED, height=30)
-button = Button(text="Delete Other Tweets", bg = "#%02x%02x%02x" % (228, 190, 190), command=handle_button_click)
+    print("Please wait while we gather your Twitter activity...")
 
-i = 0
-for twt in tweet_list:
-    i += 1
-    type = "tweet"
-    if twt["retweet"]:
-        type = "retweet"
-    if twt["like"]:
-        type = "like"
+    tweet_list = []
+    for tweet in timeline:
+        if tweet.created_at <= cutoff_date:
+            info = {
+            "tweet_id": tweet.id,
+            "tweet_text": tweet.text,
+            "tweet_created": tweet.created_at.strftime('%m/%d/%Y'),
+            "retweet": tweet.retweeted,
+            "like": tweet.favorited,
+            "tweet_delete": True
+            }
+            tweet_list.append(info)
 
-    tweet_list_item = str(twt["tweet_id"]) + " (" + type + "): " + twt["tweet_text"]
+    # prompt user for which tweets/retweets/likes to keep
+    master = Tk()
 
-    # strip out extra unrecognized characters from tweet - step need for tkinter gui
-    char_list = [tweet_list_item[j] for j in range(len(tweet_list_item)) if ord(tweet_list_item[j]) in range(65536)]
-    tweet_list_item=""
-    for j in char_list:
-        tweet_list_item=tweet_list_item+j
-    listbox.insert(i, tweet_list_item)
+    listbox_label = Label(text="Select the twitter activity to KEEP:\n (all unselected tweets, retweets and likes will be deleted)", width=150)
+    listbox = Listbox(selectmode=EXTENDED, height=30)
+    button = Button(text="Delete Other Tweets", bg = "#%02x%02x%02x" % (228, 190, 190), command=handle_button_click)
 
-listbox_label.pack()
-listbox.pack(side=LEFT, fill=BOTH, expand=1)
-button.pack()
-master.protocol("WM_DELETE_WINDOW", on_closing)
-master.mainloop()
+    i = 0
+    for twt in tweet_list:
+        i += 1
+        type = "tweet"
+        if twt["retweet"]:
+            type = "retweet"
+        if twt["like"]:
+            type = "like"
 
-print("Thank you for using the EraseMe service!")
+        tweet_list_item = str(twt["tweet_id"]) + " (" + type + "): " + twt["tweet_text"]
+
+        # strip out extra unrecognized characters from tweet - step need for tkinter gui
+        char_list = [tweet_list_item[j] for j in range(len(tweet_list_item)) if ord(tweet_list_item[j]) in range(65536)]
+        tweet_list_item=""
+        for j in char_list:
+            tweet_list_item=tweet_list_item+j
+        listbox.insert(i, tweet_list_item)
+
+    listbox_label.pack()
+    listbox.pack(side=LEFT, fill=BOTH, expand=1)
+    button.pack()
+    master.protocol("WM_DELETE_WINDOW", on_closing)
+    master.mainloop()
+
+    print("Thank you for using the EraseMe service!")
